d6e223f0800accf9974315d0f53f7ee0ff89cbeb
cheolwo <powerqkrcjfd@gmail.com>
2021-03-01 17:46:35 +0900

    Create IDataManager.cs

---
+++ Network/Network/Warehouse/IDataManager/IDataManager.cs
@@ -0,0 +1,28 @@
+namespace Warehouse.IDataManager
+{
+  public interface IWarehouseDataManager
+  {
+  }
+  
+  public interface IWCommodityDataManager
+  {
+  }
+  
+  public interface DividedCommodityDataManager
+  {
+  }
+  
+  public interface ILoadFrameDataManager
+  {}
+  
+  public interface IImageofCommodityDataManager
+  {}
+  
+  public interface IImageofIncomingDataManager
+  {}
+  
+  public interface IImageofLoadingDataManager
+  {}
+  
+  
+}
