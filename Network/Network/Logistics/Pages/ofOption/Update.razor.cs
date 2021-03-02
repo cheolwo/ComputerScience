a16dd86efa9853c2a89c046da149fae2d239a467
cheolwo <powerqkrcjfd@gmail.com>
2021-03-02 09:33:22 +0900

    Update Update.razor.cs

--- Network/Network/Logistics/Pages/ofOption/Update.razor.cs
+++ Network/Network/Logistics/Pages/ofOption/Update.razor.cs
@@ -7,5 +7,44 @@ namespace Logistics.Pages.ofOption
 {
     public partial class Update
     {
+        [Inject] IOptionManager OptionManager {get; set;}
+        [Inject] ICommodityFileManager FileManager {get; set;}
+        [Inject] IImageofOptionManager ImageofOptionManager {get; set;}
+        
+        [Parameter] public string OptionNo {get; set;}
+        
+        public List<IMatFileUploadEntry> Images = new List<IMatFileUploadEntry>();
+        public Option UpdateOption = new Option();
+            
+        protected override void OnInitialized()
+        {
+            UpdateOption = OptionManager.GetById(Convert.ToInt32(OptionNo));
+            UpdateOption.Images = ImageofOptionManager.GetByOption(UpdateOption);
+        }
+        
+        public void FileUpload(IMatFileUploadEntry[] Files)
+        {
+            if (Files.Count > 0)
+            {
+                foreach(var File in Files)
+                {
+                    var Exist = Images.Find(File);
+                    if(Exist == null) { Images.Add(Exist); }
+                }
+            }
+        }
+        
+        public void Update()
+        {
+            
+        }
+        
+        public void Reset()
+        {
+            // UpdateOption.... null
+            
+            Images.Clear();
+        }
+        
     }
 }
