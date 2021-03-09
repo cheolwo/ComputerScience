c5ec663a5af26b2db78432887642a5933cf4c064
cheolwo <powerqkrcjfd@gmail.com>
2021-03-10 00:06:50 +0900

    Trade CleanUp

--- Network/Network/Trade/CommodityDataContext.cs
+++ Network/Network/Trade/CommodityDataContext.cs
@@ -4,7 +4,7 @@ using Trade.Model;
 
 namespace Trade
 {
-    public class CommodityDataContext : DbContext
+    public class TCommodityDataContext : DbContext
     {
         public CommodityDataContext(DbContextOptions options)
             : base(options)
@@ -12,23 +12,15 @@ namespace Trade
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Commodity>()
-                .HasOne(a => a.CommodityDetail)
-                .WithOne(b => b.Commodity)
-                .HasForeignKey<CommodityDetail>(b => b.CommodityNo);
-        }
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<Commodity>()
+        //         .HasOne(a => a.CommodityDetail)
+        //         .WithOne(b => b.Commodity)
+        //         .HasForeignKey<CommodityDetail>(b => b.CommodityNo);
+        // }
 
-        public DbSet<Commodity> Commodities { get; set; }
-        public DbSet<CommodityDetail> CommodityDetails { get; set; }
-        public DbSet<Doc> Docs { get; set; }
-        public DbSet<ImageofDetail> ImageofDetails { get; set; }
-        public DbSet<ImageofOption> ImageofOptions { get; set; }
-        public DbSet<Option> Options { get; set; }
-        //public DbSet<CompanyofBuying> CompanyofBuyings { get; set; }
-        //public DbSet<Buying> Buyings { get; set; }
-        public DbSet<ImageofCommodity> ImageofCommodities { get; set; }
-        public DbSet<DetailImage> DetailImages { get; set; }
+        public DbSet<TCommodity> TCommodities { get; set; }
+        public DbSet<DocofTCommodity> DocsofTCommodity { get; set; }}
     }
 }
